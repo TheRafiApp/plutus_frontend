d198c67c74391867251fc2a8c4c8c825a4999ba5
Nick Ford <nickforddesign@gmail.com>
2017-01-09 14:58:08 -0500

    deploying to prod

--- js/config.build.js
+++ js/config.build.js
@@ -8,7 +8,7 @@ require.config({
   waitSeconds : 60,
 
   // base url for modules
-  baseUrl: location.origin + '/js/build/1483736088/',
+  baseUrl: location.origin + '/js/build/1483991860/',
 
   paths: {
     'jquery':               'vendor/jquery.min',
